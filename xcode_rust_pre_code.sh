7c33d5f7e7232d0c813cc9c3dae35f4c37a9f31f
ValAudi <33517863+ValAudi@users.noreply.github.com>
2023-04-06 12:12:49 -0500

    Cleaning up pre build script

--- xcode_rust_pre_code.sh
+++ xcode_rust_pre_code.sh
@@ -1,8 +1,8 @@
 set -e
-PATH="$PATH:/Users/pamelaaudi/.cargo/bin"
+PATH="$PATH:.../.cargo/bin"
 
 # Navigate to directory
-cd /Users/pamelaaudi/Documents/CodeTemplates/Rust-Swift/net_link
+cd .../Rust-Swift/net_link
 
 # Build the programs
 cargo build --target aarch64-apple-ios
@@ -15,8 +15,8 @@ cbindgen -l C -o target/net_link.h
 lipo -create target/aarch64-apple-ios/debug/libnet_link.a target/x86_64-apple-ios/debug/libnet_link.a -output target/libnet_link.a
 
 # copy the fatfile into the ios project
-cp /Users/pamelaaudi/Documents/CodeTemplates/Rust-Swift/net_link/target/libnet_link.a /Users/pamelaaudi/Documents/CodeTemplates/Rust-Swift/ListInterfaces/net_link/
+cp .../Rust-Swift/net_link/target/libnet_link.a .../Rust-Swift/ListInterfaces/net_link/
 
 # copy header file into iOS project
-cp /Users/pamelaaudi/Documents/CodeTemplates/Rust-Swift/net_link/target/net_link.h /Users/pamelaaudi/Documents/CodeTemplates/Rust-Swift/ListInterfaces/net_link/
+cp .../Rust-Swift/net_link/target/net_link.h .../Rust-Swift/ListInterfaces/net_link/
 # Type a script or drag a script file from your workspace to insert its path.
