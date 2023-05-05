468778a2ba292bf67932d9e23390e1a0971fa276
ValAudi <33517863+ValAudi@users.noreply.github.com>
2023-05-05 17:00:04 -0500

    Update get_interfaces.rs

--- net_link/examples/get_interfaces.rs
+++ net_link/examples/get_interfaces.rs
@@ -8,9 +8,4 @@ fn main() {
     let r = interfaces[0].name.len() as c_uint;
     println!("{:#?}", interfaces[1]);
     println!("{}", r);
-    // let l = get_interface_data_count();
-    // let v = unsafe{ std::slice::from_raw_parts(prt, l as usize) };
-    // for slice in v {
-    //     println!("{:#?}", slice);
-    // }
 }
